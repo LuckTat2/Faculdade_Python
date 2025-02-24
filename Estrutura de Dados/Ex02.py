230c012da3e605326a16d459e0dfd02d7c43f2e2
Lucas Silva <lucknroll66@outlook.com>
2025-02-24 10:55:28 -0300

    Criei pastas para os arquivos

--- Estrutura de Dados/Ex02.py
+++ Estrutura de Dados/Ex02.py
@@ -12,7 +12,7 @@ print(pilha2[-1]) #Se o índice for -1, acessamos o último valor da pilha
 
 pilha2.clear() #Limpa pilha
 
-lenI(pilha2) #Retorna a quantidade de elementos da pilha
+len(pilha2) #Retorna a quantidade de elementos da pilha
 sum(pilha2) #Soma os elementos da pilha
 max(pilha2) #Retorna o maior valor
 min(pilha2) #Retorna o menor valor
